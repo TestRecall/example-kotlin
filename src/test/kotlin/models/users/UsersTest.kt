44a38b8d69eaaed96708f2bdc95423c3706cd4e0
Andrew Klotz <agc.klotz@gmail.com>
2021-04-03 23:11:39 +0000

    add another test
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

---
+++ src/test/kotlin/models/users/UsersTest.kt
@@ -0,0 +1,12 @@
+package models.users;
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+public class UsersTest {
+
+    @Test
+    fun simplest() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2")
+    }
+}
